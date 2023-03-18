c6c4afc6fdc6c7b2e8b96e7d2b700a836f589259
bread-up <breadup096@gmail.com>
2023-03-18 11:24:18 +0100

    use template
    Signed-off-by: bread-up <breadup096@gmail.com>

--- scripts/setupbuilder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash env
-
-GCC_LINK="https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9"
-
-if [[ "$BUILDER" == "gcc"  ]]; then
-    git clone --depth=1 $GCC_LINK /opt/gcc
-fi
