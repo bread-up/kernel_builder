75e3cec5d1def4fc3dab757a28c102b8fe745188
bread-up <breadup096@gmail.com>
2023-03-18 16:29:19 +0100

    workflow: add defconfig maker function
    Signed-off-by: bread-up <breadup096@gmail.com>
    
    args added:
    ARCH to specify your arch "arm, arm64 ..."
    DEFCONFIG to specify defconfig name
    PLATFORM_VERSION to specify platform version (not api version)
    ANDROID_MAJOR_VERSION to specify the android that kernel is builded

--- config.sh
+++ config.sh
@@ -5,5 +5,11 @@ export KERNEL_SOURCE="https://github.com/bread-up/android_kernel_samsung_exynos7
 export SOURCE_BRANCH="nethunter"
 
 # BUILDER
-export BUILDER="clang"
-export SDCLANG="true"
+export BUILDER="gcc"
+#export SDCLANG="false"
+
+# SPECS
+export DEFCONFIG=nh-m10lte_defconfig
+export ARCH=arm64
+export PLATFORM_VERSION=10
+export ANDROID_MAJOR_VERSION=q
