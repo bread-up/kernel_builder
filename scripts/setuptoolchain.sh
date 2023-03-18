4f1230bb95a68bef22ef9a96f8694f4c17ab3afd
bread-up <breadup096@gmail.com>
2023-03-18 12:04:54 +0100

    setuptoolchain: fix undeclared argument
    Signed-off-by: bread-up <breadup096@gmail.com>

--- scripts/setuptoolchain.sh
+++ scripts/setuptoolchain.sh
@@ -3,6 +3,6 @@
 T_GCC="https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9"
 
 if [ $BUILDER == "gcc" ]; then
-  git clone --depth=1 T_GCC /opt/gcc
+  git clone --depth=1 $T_GCC /opt/gcc
 fi
 
