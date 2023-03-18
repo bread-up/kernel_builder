704e742d2d05d366a19ba0a985b753294be4d3d5
bread-up <breadup096@gmail.com>
2023-03-18 11:47:07 +0100

    config: use export to export args
    Signed-off-by: bread-up <breadup096@gmail.com>

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash env
 
 # SOURCE
-KERNEL_SOURCE="https://github.com/bread-up/android_kernel_samsung_exynos7870"
-SOURCE_BRANCH="nethunter"
+export KERNEL_SOURCE="https://github.com/bread-up/android_kernel_samsung_exynos7870"
+export SOURCE_BRANCH="nethunter"
+
