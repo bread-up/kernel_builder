991c29e44bf33314eeb2178c025a3d7979de8ae3
bread-up <breadup096@gmail.com>
2023-03-19 11:13:48 +0000

    makezip: enhance script
    Signed-off-by: bread-up <breadup096@gmail.com>

--- scripts/makezip.sh
+++ scripts/makezip.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-cd ..
-WORK=$(pwd)
-cd work
-
 pack_kernel () {
-   mkdir /tmp/out
-   mv arch/$ARCH/boot/$IMAGE_NAME /tmp/out
+   mkdir ../out
+   mv arch/$ARCH/boot/$IMAGE_NAME ../out
    if [ $INCLUDE_KO == true ]; then
-     mv lib/modules /tmp/out
-     cd /tmp/out
-     7z -y a kernel.zip *
-   else
-     cd /tmp/out
-     7z -y a kernel.zip *
+     mv lib/modules ../out
    fi
-   mv kernel.zip $WORK
+   cd ../out
+   7z -y a kernel.zip *
+   mv kernel.zip ..
 }
 
 if [ $USE_ANYKERNEL3 == "true" ]; then
