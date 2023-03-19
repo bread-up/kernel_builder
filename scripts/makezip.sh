2115769a3b81608e74f1c52172bb95b13695bf91
bread-up <breadup096@gmail.com>
2023-03-19 07:28:39 +0000

    makezip: add arg to specify Image name
    Signed-off-by: bread-up <breadup096@gmail.com>

--- scripts/makezip.sh
+++ scripts/makezip.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+WORK=../$(pwd)
+
 if [ $USE_ANYKERNEL3 == "true" ]; then
   echo "ok"
 else 
-  7z -y a kernel.zip arch/$ARCH/boot/Image
+  cd /tmp
+  7z -y a kernel.zip kernelpath()
+  mv kernel.zip $WORK
 fi
+
+kernelpath() {
+  NK=""
+  if [ -z ${IMAGE_NAME+x} ]; then
+   echo "Image doesnt exists, use default Image name"
+   NK="Image"
+  else
+   NK=$IMAGE_NAME
+  fi
+  mv arch/$ARCH/boot/$NK /tmp
+  return $NK
+}
