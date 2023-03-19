84d7705f52aa954eae73efc42f71fb31640219ca
bread-up <breadup096@gmail.com>
2023-03-19 10:58:28 +0000

    makezip: fix path bug
    Signed-off-by: bread-up <breadup096@gmail.com>

--- scripts/makezip.sh
+++ scripts/makezip.sh
@@ -6,9 +6,9 @@ cd work
 
 pack_kernel () {
    mkdir /tmp/out
-   mv out/arch/$ARCH/boot/$IMAGE_NAME /tmp/out
+   mv arch/$ARCH/boot/$IMAGE_NAME /tmp/out
    if [ $INCLUDE_KO == true ]; then
-     mv out/lib/modules /tmp/out
+     mv lib/modules /tmp/out
      cd /tmp/out
      7z -y a kernel.zip *
    else
