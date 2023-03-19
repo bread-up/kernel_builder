dbca576d6069f7b9424b9c0882ffe01ba13faa03
bread-up <breadup096@gmail.com>
2023-03-19 10:32:33 +0000

    makezip: fix syntax error near fi
    Signed-off-by: bread-up <breadup096@gmail.com>

--- scripts/makezip.sh
+++ scripts/makezip.sh
@@ -4,13 +4,7 @@ cd ..
 WORK=$(pwd)
 cd work
 
-if [ $USE_ANYKERNEL3 == "true" ]; then
-  echo "ok"
-else
-  pack_kernel()
-fi
-
-pack_kernel() {
+pack_kernel () {
    mkdir /tmp/out
    mv out/arch/$ARCH/boot/$IMAGE_NAME /tmp/out
    if [ $INCLUDE_KO == true ]; then
@@ -23,3 +17,9 @@ pack_kernel() {
    fi
    mv kernel.zip $WORK
 }
+
+if [ $USE_ANYKERNEL3 == "true" ]; then
+  AK=true
+else
+  pack_kernel
+fi
